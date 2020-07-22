8d824c024a479a7842b9c25a8d889bd86d90258a
Luis Vega <vegaluisjose@gmail.com>
2020-07-21 18:44:16 -0700

    add type helpers

--- src/v17/helpers.rs
+++ src/v17/helpers.rs
@@ -1,6 +1,15 @@
 use crate::v17::ast::*;
 
 impl Ty {
+    pub fn new_int() -> Ty {
+        Ty::Int
+    }
+
+    pub fn new_width(width: u64) -> Ty {
+        assert!(width > 0, "Error: width must be greater than zero");
+        Ty::Width(width)
+    }
+
     pub fn width(&self) -> u64 {
         match self {
             Ty::Width(w) => w.clone(),
