2a9249995130ecfd117bb165b9bbb01141c2a9a5
Rachit Nigam <rachit.nigam12@gmail.com>
2020-10-23 15:50:44 -0400

    fix tests

--- regression/v17/module_with_function_add_one.v
+++ regression/v17/module_with_function_add_one.v
@@ -2,7 +2,7 @@ module module_with_function_add_one ();
     function int add_one(input logic [31:0] val);
         logic [31:0] res;
         begin
-            res = val + 1'b1;
+            res = (val + 1'b1);
             return res;
         end
     endfunction
